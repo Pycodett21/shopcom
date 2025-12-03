ee44607838ad08c4cb7b2406ca5548035594eaa8
Pycodett21 <arafatjemal2015@gmail.com>
2025-12-03 04:33:47 -0800

    commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'category',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'category',
+   
+    
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
